0edfae1de9df7a2f5b29d6eafde3b54dc5202796
quang <quang.hoang@enouvo.com>
2016-12-12 11:05:06 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Table from 'antd/lib/table';
+import logo from './logo.svg';
+import './App.css';
+
+const dataSource = [{
+  key: '1',
+  name: 'Mike',
+  age: 32,
+  address: '10 Downing Street'
+}, {
+  key: '2',
+  name: 'John',
+  age: 42,
+  address: '10 Downing Street'
+}];
+
+const columns = [{
+  title: 'Name',
+  dataIndex: 'name',
+  key: 'name',
+}, {
+  title: 'Age',
+  dataIndex: 'age',
+  key: 'age',
+}, {
+  title: 'Address',
+  dataIndex: 'address',
+  key: 'address',
+}];
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Table dataSource={dataSource} columns={columns} />
+      </div>
+    );
+  }
+}
+
+export default App;
